c9aa9b17bdf599677e349df85f6ec05536294270
Luis Inacio <lrs.inacio@gmail.com>
2022-06-22 19:02:09 +0100

    first commit

---
+++ Restaurant.Services.ProductsAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.Services.ProductsAPI.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
